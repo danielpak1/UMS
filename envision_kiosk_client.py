3639d0568415e614c03dbd4d1489b87c69e6cf73
EnVision Maker Studio <43390906+envisionUCSD@users.noreply.github.com>
2018-09-21 19:25:59 -0700

    Admin override (#1)
    Able to use an admin card to bypass a request to the server to verify ID. Still need to implement button choices in admin mode:
    
        If initial connection fails (unlikely, but still possible) then images aren't loaded to buttons
        -- Could load a default?
        Printer kiosk functionality is broken anyway if kiosk can't connect to envision server
        laptop kiosk, on the other hand, can still open lockers so this has some use
    
    Future updates:
    Verify laptop kiosk can open doors in admin mode with bypass
    Preload default images in case of server fail on initial connect
    Add admin "System Control" button for and

--- envision_kiosk_client.py
+++ envision_kiosk_client.py
@@ -15,7 +15,7 @@ else:
 	GTK = True
 	MSW = False
 
-VERSION = "396"
+VERSION = "398"
 """
 -286: updated popupFrame.onEnter to stop the idle timer. This isn't the correct solution but works for now
 	: need to have idleFrame.onExit clean up all of the modal dialogs before exiting.
@@ -40,6 +40,7 @@ inputList = []
 #standard UCSD id length, including leading and trailing '$' from mag-reader
 idLength = 11
 IDLETIME = 30000
+
 
 PORTSAVAIL = list(serial.tools.list_ports.comports())
 for port in PORTSAVAIL:
@@ -609,6 +610,7 @@ class MainWindow(wx.Frame):
 		wx.Frame.__init__(self, parent, title = title, style=styleFlags)
 		self.adminMode = False
 		self.bi = None
+		self.BYPASS = False
 		#bind a close event to the function
 		self.Bind(wx.EVT_CLOSE, self.OnClose)
 		#sizers for centering the static text 
@@ -839,16 +841,18 @@ class MainWindow(wx.Frame):
 			wx.MessageBox("Please Use The ID-Reader", "ERROR")
 			return
 		#ascii code 36 is $ and is the start and trail char of the magreader
-		elif keycode == 36:
+		#ascii code 35 is a hash (#) and indicates an admin card is being used
+		elif keycode == 36 or keycode == 35:
 			#if present, start accepting characters into the inputList
 			acceptString = True
 			inputList.append(keycode)
 			return
+		
 		#look for an enter key
 		if acceptString:
 			if keycode == wx.WXK_RETURN:
-				#if return is pressed, make sure the last character is $
-				if inputList[-1] == 36:
+				#if return is pressed, make sure the last character is $ (or a hash [#] for adminCard)
+				if inputList[-1] == 36 or inputList[-1] == 35:
 					#join the character together in a string
 					inputString = ''.join(chr(i) for i in inputList)
 					#wx.MessageBox("You Entered \n"+inputString, "ERROR")
@@ -872,7 +876,7 @@ class MainWindow(wx.Frame):
 	def idEnter(self, idInput):
 		idString = ""
 		if (idInput.startswith('$') and idInput.endswith('$')):
-		#check once more if the string is correclty formatted
+		#check once more if the string is correctly formatted
 			idChars = list(idInput)
 			if idChars[2] == '9':
 			#magstripe reads a '09' for students, replace this with a 'A' per UCSD standards
@@ -889,6 +893,20 @@ class MainWindow(wx.Frame):
 
 			self.userIDnumber = idString #set the current user to this ID string
 			self.socketWorker.sendEvent(["EVT_CHECKID",MACHINENAME,self.userIDnumber,"False"]) #check the ID record on the server
+		
+		elif (idInput.startswith('#') and idInput.endswith('#')):
+		#check once more if the string is correctly formatted
+			self.userIDnumber = idString
+			self.BYPASS = True
+			app.frame.ShowFullScreen(True)
+			app.frame.Show()
+			for i in xrange(NUMPRINTERS):
+				app.frame.bitmap_buttons[i].Disable()
+			app.frame.bitmap_buttons[-1].Enable()
+			app.frame.cancelBtn.Disable()
+			self.Hide()
+			app.frame.timer.inactiveCount = 0
+			app.frame.timer.Start(IDLETIME)
 		else:
 			self.SetFocus()
 			return
@@ -1316,6 +1334,12 @@ class PrinterFrame(wx.Frame):
 	def closeAdmin(self,event):
 		app.frame.timer.Stop()
 		app.signOnFrame.adminMode = False
+		if app.signOnFrame.BYPASS:
+			for i in xrange(NUMPRINTERS):
+				thisButton = app.frame.bitmap_buttons[i]
+				if thisButton.status == "ENABLED":
+					thisButton.Enable()
+			app.signOnFrame.BYPASS = False
 		app.frame.bitmap_buttons[-1].Disable()
 		app.signOnFrame.Show()
 		app.signOnFrame.Raise()

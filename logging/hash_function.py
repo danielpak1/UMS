7f47ecdd19b22d1dca362f666917c1d447c06040
JD <envision@ucsd.edu>
2020-02-05 11:30:38 -0800

    updated quarter/year salt

--- logging/hash_function.py
+++ logging/hash_function.py
@@ -30,7 +30,7 @@ for cls in result:
 
 #print classList
 for cls in classList:
-	codeString = cls[0]+cls[1]+'-'+'FQ19'
+	codeString = cls[0]+cls[1]+'-'+'WQ20'
 	code = str(int(hashlib.md5(codeString.upper()).hexdigest(),16) % (10 ** 6)).zfill(6)
 	cls[2]=code
 	query = 'UPDATE classes SET code="'+code+'" WHERE department="'+cls[0]+'" AND number="'+cls[1]+'"'
